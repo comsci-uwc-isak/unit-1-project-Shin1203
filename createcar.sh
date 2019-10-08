2826abf49c35f6bce375e183c612f53fc412d7c0
Shin1203 <2021.shinnosuke.miyanaga@uwcisak.jp>
2019-10-08 22:50:42 +0900

    Added testing for delete and install

--- createcar.sh
+++ createcar.sh
@@ -3,7 +3,7 @@
 #reading arguments, making sure there are 4
 if [ $# -eq 4 ]; then
 	#access car storage, make txt file with info
-	echo "$1 , $2 , $3 , $4 " >> ~/Desktop/CarRentalApp/car/maincarfile.txt
+	echo "$1 $2 $3 $4" >> ~/Desktop/CarRentalApp/car/maincarfile.txt
 	echo " " > ~/Desktop/CarRentalApp/car/"$1.txt"
 	bash ~/Desktop/CarRentalApp/frame "Car created successfully"
 else
--- deletecar.sh
+++ deletecar.sh
@@ -3,8 +3,9 @@
 #reading arguments, making sure there are 4
 if [ $# -eq 1 ]; then
 	#access maincarfile.txt, delete line that includes argument
-	sed -i "/$1/d" car/maincarfile.txt
-	rm 
+	sed -i "/$1/d" ~/Desktop/CarRentalApp/car/maincarfile.txt
+	#remove car file 
+	rm -r ~/Desktop/CarRentalApp/car/"$1".txt 
 else
 	#exit messaage
 	echo "please enter liscence plate of car"
